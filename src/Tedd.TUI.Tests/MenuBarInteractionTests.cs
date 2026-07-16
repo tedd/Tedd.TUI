8f64967f3e0f9fe3f3dbd7985e72c7baded0b46d
Tedd Hansen <tedd@konge.net>
2026-07-16 05:49:47 +0200

    test: route nested menu popup clicks

--- src/Tedd.TUI.Tests/MenuBarInteractionTests.cs
+++ src/Tedd.TUI.Tests/MenuBarInteractionTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using Tedd.TUI;
+using Tedd.TUI.Tests.TestInfrastructure;
 
 namespace Tedd.TUI.Tests;
 
@@ -37,5 +38,63 @@ public class MenuBarInteractionTests
         Assert.NotNull(hit);
         // InputHitTest returns the leaf node (TextBlock), so we verify it's the child of our menu item
         Assert.Equal(fileMenu, hit.Element.Parent);
+    }
+
+    [Fact]
+    public void MouseClick_NestedMenuBar_InvokesOnlyChosenPopupItem()
+    {
+        var openCount = 0;
+        var cutCount = 0;
+        var openItem = new MenuItem
+        {
+            Header = new TextBlock { Text = "Open" },
+            Command = () => openCount++
+        };
+        var cutItem = new MenuItem
+        {
+            Header = new TextBlock { Text = "Cut" },
+            Command = () => cutCount++
+        };
+        var fileMenu = new MenuItem { Header = new TextBlock { Text = "File" } };
+        fileMenu.Items.Add(openItem);
+        var editMenu = new MenuItem { Header = new TextBlock { Text = "Edit" } };
+        editMenu.Items.Add(cutItem);
+        var spacer = new TextBlock { Text = "   " };
+        var menuBar = new MenuBar();
+        menuBar.AddChild(fileMenu);
+        menuBar.AddChild(spacer);
+        menuBar.AddChild(editMenu);
+
+        var panel = new StackPanel();
+        panel.AddChild(new TextBlock { Text = "application" });
+        panel.AddChild(menuBar);
+        panel.AddChild(new TextBlock { Text = "workspace surface" });
+        var host = new ControlTestHost(new Border { Child = panel }, 28, 8);
+
+        host.Click(spacer, 1, 0);
+        Assert.Equal(0, openCount);
+        Assert.Equal(0, cutCount);
+
+        var fileClick = host.Click(fileMenu, 1, 0);
+
+        Assert.True(fileClick.Down.Handled);
+        Assert.True(fileMenu.IsExpanded);
+        Assert.False(editMenu.IsExpanded);
+
+        var openClick = host.Click(openItem, 1, 0);
+
+        Assert.True(openClick.Down.Handled);
+        Assert.Equal(1, openCount);
+        Assert.Equal(0, cutCount);
+        Assert.False(fileMenu.IsExpanded);
+
+        host.Click(editMenu, 1, 0);
+        Assert.False(fileMenu.IsExpanded);
+        Assert.True(editMenu.IsExpanded);
+
+        host.Click(cutItem, 1, 0);
+        Assert.Equal(1, openCount);
+        Assert.Equal(1, cutCount);
+        Assert.False(editMenu.IsExpanded);
     }
 }
